3878f338a2c1068cd590c6d72f06d274beedf3ee
andreacassini <andrea1316@hotmail.it>
2023-06-28 17:59:51 +0200

    first commit, maxrostructure

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<script>
+  import AppHeader from './components/AppHeader.vue';
+  import AppMain from './components/AppMain.vue';
+  import { store } from './store.js'; 
+
+  export default {
+    data() {
+      return {
+        store,
+      }
+    },
+    components: {
+      AppHeader,
+      AppMain,
+    }
+  }
+</script>
+
+<template>
+    <AppHeader />
+    <AppMain />
+</template>
+
+<style lang="scss">
+  @use '../src/styles/generals.scss' as *;
+
+</style>
---
+++ src/components/AppMain.vue
@@ -0,0 +1,25 @@
+<script>
+  import { store } from '../store.js';
+  import AppCard from './AppCard.vue';
+
+  export default {
+    components: {
+      AppCard
+    },
+    data() {
+      return {
+        store,
+      }
+    },
+}
+</script>
+
+<template>
+  <div>
+    
+  </div>
+</template>
+
+<style lang="scss" scoped>
+
+</style>
