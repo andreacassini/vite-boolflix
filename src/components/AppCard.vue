df38f19a8489fc877e05d562cd9c64096495cba7
andreacassini <andrea1316@hotmail.it>
2023-06-29 14:28:50 +0200

    Add img with url

--- src/components/AppCard.vue
+++ src/components/AppCard.vue
@@ -1,6 +1,4 @@
 <script>
-
-import { faHardOfHearing } from '@fortawesome/free-solid-svg-icons';
 import { store } from '../store.js';
 export default {
     data() {
@@ -14,17 +12,15 @@ export default {
 }
 </script>
 <template>
-    <link href="../assets/">
-<h2 class="mb-3">{{ film.title || film.original_name }}</h2>
-<h4 class="mb-3">{{ film.original_title || film.original_name}}</h4>
-<h6 class="mb-3">Lang: <img :src="'../../public/' + film.original_language + '_16.png'" :alt="film.original_language"></h6>
-<p><strong>{{ film.vote_average }}</strong></p>
-<p>Category: {{ film.media_type }}</p>
+    <img :src="store.url_image + film.poster_path"> 
+    <h2 class="mb-3">{{ film.title || film.original_name }}</h2>
+    <h4 class="mb-3">{{ film.original_title || film.original_name}}</h4>
+    <h6 class="mb-3">Lang: <img :src="'../../public/' + film.original_language + '_16.png'" :alt="film.original_language"></h6>
+    <p><strong>{{ film.vote_average }}</strong></p>
+    <p>Category: {{ film.media_type }}</p>
 
 </template>
 
 <style lang="scss">
-img{
-    width: 10%;
-}
+
 </style>
--- src/store.js
+++ src/store.js
@@ -2,5 +2,6 @@ import { reactive } from 'vue';
 export const store = reactive({
     apiUrl: 'https://api.themoviedb.org/3/search/multi?api_key=0cfb28bbc7ccb0a51f114539b56f21c7',
     list: [],
-    search: ''
+    search: '',
+    url_image: 'https://image.tmdb.org/t/p/w200/'
 });
