11edc50a339275b4f20a8ffe2d7be887a7577e5a
andreacassini <andrea1316@hotmail.it>
2023-06-29 13:51:39 +0200

    Add categories and img flag but not working

--- src/components/AppCard.vue
+++ src/components/AppCard.vue
@@ -1,4 +1,6 @@
 <script>
+
+import { faHardOfHearing } from '@fortawesome/free-solid-svg-icons';
 import { store } from '../store.js';
 export default {
     data() {
@@ -12,13 +14,17 @@ export default {
 }
 </script>
 <template>
-<h2 class="mb-3">{{ film.title }}</h2>
-<h4 class="mb-3">{{ film.original_title }}</h4>
-<h6 class="mb-3">{{ film.original_language }}</h6>
+    <link href="../assets/">
+<h2 class="mb-3">{{ film.title || film.original_name }}</h2>
+<h4 class="mb-3">{{ film.original_title || film.original_name}}</h4>
+<h6 class="mb-3">Lang: <img :src="'../assets/flags/' + film.original_language + '_16.png'"></h6>
 <p><strong>{{ film.vote_average }}</strong></p>
+<p>Category: {{ film.media_type }}</p>
 
 </template>
 
 <style lang="scss">
-
+img{
+    width: 100%;
+}
 </style>
