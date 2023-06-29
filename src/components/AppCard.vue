61aac93071d876220b5bb8654bee0f6a30994ce9
andreacassini <andrea1316@hotmail.it>
2023-06-29 16:22:13 +0200

    hover img

--- src/components/AppCard.vue
+++ src/components/AppCard.vue
@@ -13,17 +13,44 @@ export default {
 }
 </script>
 <template>
-    <img :src="store.url_image + film.poster_path" class="my-3">
-    <div>    <!-- CONTENT -->
-        <h2 class="mb-3">{{ film.title || film.original_name }}</h2>
-        <h4 class="mb-3">{{ film.original_title || film.original_name}}</h4>
-        <h6 class="mb-3">Lang: <img :src="'../../public/' + film.original_language + '_16.png'" :alt="film.original_language"></h6>
-        <p><strong>{{ film.vote_average }}</strong></p>
-        <p>Category: {{ film.media_type }}</p>
+    <div class="card-personal">
+        <img :src="store.url_image + film.poster_path" id="img-card">
+       <!-- CONTENT -->
+       <div class="content">
+            <h2 class="mb-3">{{ film.title || film.original_name }}</h2>
+            <h4 class="mb-3">{{ film.original_title || film.original_name}}</h4>
+            <h6 class="mb-3">Lang: <img :src="'../../public/' + film.original_language + '_16.png'" :alt="film.original_language"></h6>
+            <p><strong>{{ film.vote_average }}</strong></p>
+            <p>Category: {{ film.media_type }}</p>
+       </div>
+       
     </div> 
    
 </template>
 
 <style lang="scss">
+.card-personal{
+    position: relative;
+    margin: 1em;
+    #img-card{
+        aspect-ratio: 2/3;
+        width: 376px
+    }
+}
+
+.card-personal:hover .content{
+    display: block;
+    background-color: rgba(0, 0, 0, 0.842);
+}
+
+.content{
+    display: none;
+    position: absolute;
+    top: 0;
+    left: 0;
+    right: 0;
+    height: 100%;
+    padding: 1em;
+}
 
 </style>
